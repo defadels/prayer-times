87a35de6742a531edc6116d234a954fadf638b68
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2021-08-27 21:38:04 +0700

    Membuat file app.js dan membuat function index

---
+++ app.js
@@ -0,0 +1,9 @@
+function index() {
+    let app = document.getElementById('app');
+    let h3  = document.createElement('h3');
+    h3.innerHTML = 'Prayer Times';
+    
+    app.appendChild(h3);
+}
+
+index();
