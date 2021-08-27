1be474fddb865d81187be084fc3740c0541f5fad
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2021-08-27 21:44:44 +0700

    menambahkan permission geolocation

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+function success(position) {
+    console.log(position);
+}
+
+function error() {
+    alert('Posisi tidak dapat diakses');
+}
+
+function userLocation() {
+    if(!navigator.geolocation) {
+        alert('Geolocation tidak didukung dalam browser Anda. Silahkan gunakan browser lain');
+    } else {
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
+
+
 function index() {
     let app = document.getElementById('app');
     let h3  = document.createElement('h3');
     h3.innerHTML = 'Prayer Times';
     
     app.appendChild(h3);
+
+    userLocation();
 }
 
 index();
