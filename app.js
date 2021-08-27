7aebd5b9c131732bde8b7746880beb32a963766b
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2021-08-27 21:56:46 +0700

    Fetch API Aladhan

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+function prayerTimes(latitude, longitude) {
+    fetch('https://api.aladhan.com/v1/calendar?latitude='+latitude+'&longitude='+longitude+'&method=2')
+    .then(response => response.json())
+    .then(function(response) {
+        console.log(response.data[0].timings);
+    });
+}
+
 function success(position) {
-    console.log(position);
+    prayerTimes(position.coords.latitude, position.coords.longitude);
 }
 
 function error() {
