312663f0700589ac1990253aa11e2002c3a79c70
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2021-08-29 20:34:07 +0700

    membuat tabel untuk jadwal sholat

--- app.js
+++ app.js
@@ -2,7 +2,29 @@ function prayerTimes(latitude, longitude) {
     fetch('https://api.aladhan.com/v1/calendar?latitude='+latitude+'&longitude='+longitude+'&method=2')
     .then(response => response.json())
     .then(function(response) {
-        console.log(response.data[0].timings);
+
+        let date  = new Date();
+        let today = date.getDate();
+        let data  = response.data[0].timings;
+        
+        let app         = getElementById('app');
+        let table       = createElement('table');
+        let tableTbody  = createElement('tbody');
+
+        for(i in data) {
+          let row        = tableTbody.insertRow();
+          let name       = row.insertCell(0);
+          let time       = row.insertCell(1);
+          name.innerHTML = i;
+          time.innerHTML = data[i];
+          tableTbody.appendChild(row); 
+        }
+
+        table.appendChild(tableTbody);
+        app.appendChild(table);
+
+        
+        // console.log(response.data[today]);
     });
 }
 
